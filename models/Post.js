aabd752bb239e7d90f72487978861115001b4e06
BillStephens2022 <two4onebill@yahoo.com>
2023-01-11 08:52:41 -0500

    set up 'Post' Sequelize Model

---
+++ models/Post.js
@@ -0,0 +1,40 @@
+const {Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    post_title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // This column will store a reference of the `id` of the `User` that made the 'Post'.
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
+);
+
+module.exports = Post;
