a2b5541efc08346eda40a7cc463b5a753fcd85a8
BillStephens2022 <two4onebill@yahoo.com>
2023-01-13 10:00:49 -0500

    added functionality to get single post with comment, need to fix bug

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
-const { Post } = require('../../models');
+const { Post, User } = require('../../models');
+const withAuth = require('../../utils/auth');
+const { format_date } = require('../../utils/helpers');
 
 router.get('/', (req, res) => {
     console.log('GET request received!');
@@ -15,6 +17,34 @@ router.post('/', async (req, res) => {
         user_id: req.session.user_id
     })
     res.json(newPostData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', withAuth, async (req, res) => {
+  console.log("single post requested!");
+  console.log("Post ID:" + req.params.id);
+  
+  try {
+    // Find the logged in user based on the post ID
+    const singlePostData = await Post.findByPk(req.params.id, {
+      attributes: ['id', 'post_title', 'post_content', 'date_created'],
+        include: [
+          { 
+            model: User,
+            attributes: [{exclude: password}],
+          },
+          {
+            model: Comment
+          }
+        ],
+    });
+
+    const post = singlePostData.get({ plain: true });
+    console.log(post);
+    res.render('singlePost', post, {logged_in: req.session.logged_in});
+    
   } catch (err) {
     res.status(500).json(err);
   }
