9874545bb573f1f91dddbc217746ce90102a1847
BillStephens2022 <two4onebill@yahoo.com>
2023-01-14 21:08:17 -0500

    sequelize changes for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,21 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let database = process.env.DATABASE_URL || 'tech_blog_db';
+let sequelize;
 
-const sequelize = new Sequelize(
-  database,
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+  process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306,
-  }
+  },
 );
+}
 
 module.exports = sequelize;
